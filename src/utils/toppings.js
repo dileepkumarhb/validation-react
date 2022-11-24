f0d94f6a2bf29470a3422d03266180bbbfe67304
Dileepkumar Hanamant Bannikatti <DILEEPKUMAR.BANNIKATTI@kohler.com>
2022-11-24 19:28:45 +0530

    react validation

---
+++ src/utils/toppings.js
@@ -0,0 +1,30 @@
+export const toppings = [
+  {
+    name: "Capsicum",
+
+  },
+  {
+    name: "Paneer",
+
+  },
+  {
+    name: "Red Paprika",
+
+  },
+  {
+    name: "Onions",
+
+  },
+  {
+    name: "Extra Cheese",
+
+  },
+  {
+    name: "Baby Corns",
+
+  },
+  {
+    name: "Mushroom",
+
+  }
+];
